07f271d5ddca9ce0f406286fcf7de94abd44d9fc
magdalena-b <magdexxa@gmail.com>
2020-06-03 00:14:57 +0200

    add templates

--- gym/models.py
+++ gym/models.py
@@ -14,3 +14,10 @@ class Classes(models.Model):
   trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
   date = models.CharField(max_length=250)
   name = models.CharField(max_length=250)
+
+
+class User(models.Model):
+  name = models.CharField(max_length=250)
+  surname = models.CharField(max_length=250)
+  email = models.CharField(max_length=250)
+  phone = models.CharField(max_length=250)
